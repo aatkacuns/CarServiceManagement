870f50a9a1c75ba928b073a5a74988c32f9354c3
aatkacuns <andrejs.atkacuns@visma.com>
2021-03-19 11:14:09 +0200

    no message

---
+++ CarServiceManagement/Repositories/Interfaces/IClientRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using CarServiceManagement.Models;
+
+namespace CarServiceManagement.Repositories.Interfaces
+{
+    public interface IClientRepository
+    {
+        Task<IEnumerable<Client>> GetAllClientsAsync();
+        Task<Client> GetClientByIdAsync(int id);
+        Task<int> UpdateClientInformation(Client entity);
+        Task<int> DeleteClientByIdAsync(int id);
+        Task<int> AddClientAsync(Client entity);
+    }
+}
---
+++ CarServiceManagement/Repositories/Interfaces/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using CarServiceManagement.Models;
+
+namespace CarServiceManagement.Repositories.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<IEnumerable<User>> GetAllUsersAsync();
+        Task<User> GetUserByIdAsync(int id);
+        Task<int> UpdateUserInformation(User entity);
+        Task<int> DeleteUserByIdAsync(int id);
+        Task<int> AddUserAsync(User entity);
+    }
+}
